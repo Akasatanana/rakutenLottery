ab48a25d17a663b41f56252cfcad58a47b466aba
akasata nana <akasantanana0894@gmail.com>
2022-08-04 14:46:00 +0900

    初めてコミット

---
+++ html/lottery.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>楽天宝くじ！</title>
+</head>
+
+<body>
+  <form method="POST" action="result.php">
+    <input type="number" name="num"><br>
+    <input type="submit" name="submit" value="送信">
+  </form>
+</body>
+
+</html>
